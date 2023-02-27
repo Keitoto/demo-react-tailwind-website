a04b8a85595563ef2cf53c03dd62cf9bf4378543
Keitoto <keitasekihara@gmail.com>
2023-02-27 18:56:11 +0900

    ✨ Added basic layout

---
+++ src/components/index.ts
@@ -0,0 +1,23 @@
+import Billing from '@/components/Billing';
+import Business from '@/components/Business';
+import CardDeal from '@/components/CardDeal';
+import Clients from '@/components/Clients';
+import CTA from '@/components/CTA';
+import Footer from '@/components/Footer';
+import Hero from '@/components/Hero';
+import Navbar from '@/components/Navbar';
+import Stats from '@/components/Stats';
+import Testimonials from '@/components/Testimonials';
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
