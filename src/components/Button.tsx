4e5174c187a1530a032ac09a0264eef718fad5f2
Keitoto <keitasekihara@gmail.com>
2023-02-27 20:12:50 +0900

    ✨ Added Business section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { FC } from 'react';
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
+interface IProps {
+  styles: string;
 }
+const Button: FC<IProps> = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
+};
 
-export default Button
+export default Button;
